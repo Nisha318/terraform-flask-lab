0229bb8cccb5b0c69fea75ff74cef118bc6cabd4
nisha318 <eunishap@gmail.com>
2026-02-19 14:34:55 -0600

    update tfstate backend

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "tfstate-flask-lab"
+    key    = "flask-lab/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 # Fetch the latest Amazon Linux 2023 AMI
 data "aws_ami" "amazon_linux" {
   most_recent = true
